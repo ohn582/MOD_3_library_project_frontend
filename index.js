608993dbe86b5ae2887696e0a154568e9529c7ab
ohn582 <49073115+ohn582@users.noreply.github.com>
2019-11-12 16:03:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ let bookShow = document.getElementById("discription")
 
 // allows to show a list of books
 fetch("http://localhost:3000/books")
-    .then(response => response.json())
-    .then((booksArr) => {
-        booksArr.forEach(book => {
-            let bookUl = document.createElement("ul")
-            bookUl.innerHTML = `<div class="column" style="background-color:#aaa;"><center>
+.then(response => response.json())
+.then((booksArr) => {
+    booksArr.forEach(book => {
+        let bookUl = document.createElement("ul")
+        bookUl.innerHTML = `<div class="column" style="background-color:#aaa;"><center>
         <img src=${book.image} width="200" height="300">
         <h3>${book.title}</h3>
         <button id="mybutton" data-id="${book.id}">Details</button>
         </div>
         </center>`
-            books.append(bookUl)
-        })
+        books.append(bookUl)  
+    })
 
 
-        // shows a an individual book when clicked
-        books.addEventListener("click", (evt) => {
-            evt.preventDefault();
-            console.log("hi")
-            let id = evt.target.dataset.id
+    // shows a an individual book when clicked
+    books.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        console.log("hi")
+        let id = evt.target.dataset.id
 
-            fetch(`http://localhost:3000/books/${id}`)
-                .then(response => response.json())
-                .then((book) => {
-                    // debugger
-                    bookShow.innerHTML = ""
-                    bookShow.innerHTML += `<form class="show" data-id="${book.id}" id="stuff" style="background-color:#666;">
+        fetch(`http://localhost:3000/books/${id}`)
+        .then(response => response.json())
+        .then((book) => {
+            // debugger
+            bookShow.innerHTML = ""
+            bookShow.innerHTML += `<form class="show" data-id="${book.id}" id="stuff" style="background-color:#666;">
                 <center>
                     <h1>${book.title}</h1>
                     <img src="${book.image}" width="200" height="300">
@@ -54,85 +54,87 @@ fetch("http://localhost:3000/books")
                     </div>
                 </form>`
 
-                    // List of reviews created
-                    let reviewRev1 = document.getElementById("reviews")
-                    reviewRev1.innerHTML = ""
-                    //Itterated for book
-                    book.reviews.forEach((review) => {
-                        reviewRev1.innerHTML += `
+                // List of reviews created
+                let reviewRev1 = document.getElementById("reviews")
+                reviewRev1.innerHTML = ""
+                //Itterated for book
+                book.reviews.forEach((review) => {
+                reviewRev1.innerHTML += `
                 <div id="review-${review.id}"
                 <p>${review.name}</p> 
                 <p>${review.content}<p>
                 <button id='delete'>Delete</button>
                 </div>`
-                    })
-
-
-
-                    // delete a review
-                    reviewRev1.addEventListener("click", e => {
-                        e.preventDefault();
-                        if (e.target.id === "delete") {
-                            reviewId = e.target.parentElement.parentElement.id.slice(7)
-                            fetch(`http://localhost:3000/reviews/${reviewId}`, {
-                                method: "DELETE"
-                            })
-
-                            e.target.parentElement.parentElement.remove()
-                        }
-                    })
-
-
-
-                    // create review
-                    let bookStuff = document.getElementById("stuff")
-                    bookStuff.addEventListener("submit", (e) => {
-                        e.preventDefault();
-
-                        let newName = e.target.name.value
-                        let newContent = e.target.content.value
-                        let id = e.target.dataset.id
-
-                        fetch(`http://localhost:3000/reviews`, {
-                            method: "POST",
-                            headers: {
-                                "Content-type": "application/json",
-                                "Accept": "application/json"
-                            },
-                            body: JSON.stringify({
-                                name: newName,
-                                content: newContent,
-                                book_id: parseInt(id)
-                            })
+                })
+                
+                
+                
+                // delete a review
+                reviewRev1.addEventListener("click", e => {
+                    e.preventDefault();
+                    if (e.target.id === "delete") {
+                        reviewId = e.target.parentElement.parentElement.id.slice(7)
+                        fetch(`http://localhost:3000/reviews/${reviewId}`, {
+                            method: "DELETE"
                         })
-                            .then(r => r.json())
-                            .then(reviewsArr => {
-                                if (reviewsArr.errors) {
-                                    alert("Fields cannot be blank")
-                                } else {
-                                    let reviewRev = document.getElementById("reviews")
+                        
+                        e.target.parentElement.parentElement.remove()
+                    }
+                })
 
-                                    reviewRev.innerHTML += `
+
+
+            // create review
+            let bookStuff = document.getElementById("stuff")
+            bookStuff.addEventListener("submit", (e) => {
+                e.preventDefault();
+
+                let newName = e.target.name.value
+                let newContent = e.target.content.value
+                let id = e.target.dataset.id
+    
+                fetch(`http://localhost:3000/reviews`, {
+                    method:"POST",
+                    headers: {
+                        "Content-type": "application/json",
+                        "Accept": "application/json"
+                    },
+                    body:JSON.stringify({
+                        name: newName,
+                        content: newContent,
+                        book_id: parseInt(id)
+                    })
+                })
+                .then(r => r.json())
+                .then(reviewsArr => {
+                    if (reviewsArr.errors) {
+                        alert("Fields cannot be blank")
+                    } else {
+                    let reviewRev = document.getElementById("reviews")
+
+                    reviewRev.innerHTML += `
                     <div id="review-${reviewsArr.id}"
                     <p>${reviewsArr.name}</p>
                     <p>${reviewsArr.content}</p>
                     <button id='delete'>Delete</button>
                     </div>`
-                                    bookStuff.reset()
-                                }
-                            })
-
-
-                    })
-
-
-
+                        bookStuff.reset()
+                    }
                 })
+                
+                
+            })
+
+
 
         })
-
-
+       
     })
+
+    
+})
+
+
 
 
 
